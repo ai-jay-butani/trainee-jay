b7688ef90102bb6fd31c172e8eebccbd2e75070e
ai-jay-butani <jay.butani.icreativetechnolabs.com>
2025-01-07 15:12:55 +0530

    [ADD]Session3_task

---
+++ Session3_Python/Decorator_Assignment.py
@@ -0,0 +1,73 @@
+	
+def Decorator(user_input):
+	
+	def Calculator():
+		
+		a,b = user_input()
+		
+		print("Choose + operator for Addition.")
+		print("Choose - operator for Subtraction.")
+		print("Choose * operator for Multiplication.")
+		print("Choose / operator for Divison.")
+		print("Choose ^ operator for Power.")
+		print("Choose % operator for Modulo.")
+		
+		choice = input("Enter your choice: ")
+		
+		match choice:
+			
+			case '+':
+				try:
+					print(f"Addition of {a} and {b} is: {a+b}")
+				except Exception as e:
+					print(f"{e} Exception occur.")
+					
+			case '-':
+				try:
+					print(f"Subtraction of {a} and {b} is: {a-b}")
+				except Exception as e:
+					print(f"{e} Exception occur.")
+					
+			case '*':
+				try:
+					print(f"Multiplication of {a} and {b} is: {a*b}")
+				except Exception as e:
+					print(f"{e} Exception occur.")
+					
+			case '/':
+				try:
+					print(f"Divison of {a} and {b} is: {a/b}")
+				except Exception as e:
+					print(f"{e} Exception occur.")
+			
+			case '^':
+				try:
+					print(f"Power of {a} and {b} is: {a**b}")
+				except Exception as e:
+					print(f"{e} Exception occur.")
+					
+			case '%':
+				try:
+					print(f"{a} modulo {b} is: {a%b}")
+				except Exception as e:
+					print(f"{e} Exception occur.")
+					
+			case _:
+				print("Not a Valid Option.")
+	
+	
+	return Calculator
+	
+
+@Decorator	
+def user_input():
+	num1 = eval(input("Enter First Number: "))
+	num2 = eval(input("Enter Second Number: "))
+	return num1,num2
+
+import time
+start_time = time.time()
+user_input()
+end_time = time.time()
+
+print("Execution time is: ",end_time-start_time)
