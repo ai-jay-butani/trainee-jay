e1657c5e971dca3f8ebd7b8f2faa2ca8f9b7e60f
ai-jay-butani <jay.butani.icreativetechnolabs.com>
2025-01-08 10:58:10 +0530

    [MODIFY]session3

--- Session3_Python/Decorator_Assignment.py
+++ Session3_Python/Decorator_Assignment.py
@@ -1,9 +1,6 @@
-	
 def Decorator(func):
 	
 	def Calculator():
-		
-		a,b = func()
 		
 		print("Choose + operator for Addition.")
 		print("Choose - operator for Subtraction.")
@@ -11,49 +8,63 @@ def Decorator(func):
 		print("Choose / operator for Divison.")
 		print("Choose ^ operator for Power.")
 		print("Choose % operator for Modulo.")
+		print("Choose x for Exits.")
 		
 		choice = input("Enter your choice: ")
+		
+		if choice == 'x':
+			return
+		
+		a,b = func()
 		
 		match choice:
 			
 			case '+':
 				try:
 					print(f"Addition of {a} and {b} is: {a+b}")
+					Calculator()
 				except Exception as e:
 					print(f"{e} Exception occur.")
 					
 			case '-':
 				try:
 					print(f"Subtraction of {a} and {b} is: {a-b}")
+					Calculator()
 				except Exception as e:
 					print(f"{e} Exception occur.")
 					
 			case '*':
 				try:
 					print(f"Multiplication of {a} and {b} is: {a*b}")
+					Calculator()
 				except Exception as e:
 					print(f"{e} Exception occur.")
 					
 			case '/':
 				try:
 					print(f"Divison of {a} and {b} is: {a/b}")
+					Calculator()
 				except Exception as e:
 					print(f"{e} Exception occur.")
 			
 			case '^':
 				try:
 					print(f"Power of {a} and {b} is: {a**b}")
+					Calculator()
 				except Exception as e:
 					print(f"{e} Exception occur.")
 					
 			case '%':
 				try:
 					print(f"{a} modulo {b} is: {a%b}")
+					Calculator()
 				except Exception as e:
 					print(f"{e} Exception occur.")
 					
-			case _:
+			case '_':
 				print("Not a Valid Option.")
+				Calculator()
+
 	
 	
 	return Calculator
