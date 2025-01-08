4f48ba8bbcc55b7ed6966024c8ac9d34801ab9bb
ai-jay-butani <jay.butani.icreativetechnolabs.com>
2025-01-08 18:48:31 +0530

    [ADD]session4_task

---
+++ Session4_Python/Assignment.py
@@ -0,0 +1,53 @@
+
+
+def FindFile(user_input,sub_dir,path=["/"],list1=[]):
+	
+	list_dir = list(sub_dir.keys())
+	list_dir.append("none")
+	list1.append(sub_dir)
+	
+	if user_input in list_dir:
+		path.append(user_input)
+		return ''.join(path)
+	else:
+		for i in list_dir:
+			try:
+				if i == "none":
+					del list1[-1]
+					path.pop()
+		
+					
+				elif list1[-1][i] == None:	
+					pass
+
+				else:
+					
+					path.append(i+'/')
+					sub_dir = list1[-1][i]		
+					ans = FindFile(user_input,sub_dir,path,list1)
+			except:
+				pass
+				
+			try:
+				if ans:
+					print(f"File {user_input} found at {ans}")
+					return
+			except:
+				pass				
+
+#input
+
+directory = {"documents":
+		{"work":{"report.doc":None, "file.exe":None},
+		 "personal":{"vacation.png":None, "birthday.png":None}
+		 }, 
+	     "downloads":{"file1.png":None},
+	     "songs":
+	     	{"MyFavourite":{"file2.mp3":None},
+	     	 "AllTimeFavourite":{"file3.mp3":None}
+	     	}
+	     }
+	     
+user_input = input("Please Enter filename with extension: ")
+
+FindFile(user_input,directory)	
